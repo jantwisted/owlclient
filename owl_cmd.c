f2a530759a0a723c9d0d16f871bd7f4b800d3043
jmpcode <jan.kernel@gmail.com>
2013-07-27 03:33:44 +0530

    copyright added.

--- owl_cmd.c
+++ owl_cmd.c
@@ -15,7 +15,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+   along with OwlClient.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "common.h"
 
