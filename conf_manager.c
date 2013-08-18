30a60fc1fafa016a1d3b37e3b87f511dd0c78243
jmpcode <jan.kernel@gmail.com>
2013-08-18 06:09:31 +0530

    new functions added

--- conf_manager.c
+++ conf_manager.c
@@ -17,3 +17,117 @@
    You should have received a copy of the GNU General Public License
    along with OwlClient.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#define SIZE 1000
+#define POS 11
+#define MAX_FILE_SIZE 1000
+char * get_prop(char *);
+int get_count(char *, FILE*, int *);
+char* trim_string(char*);
+void read_file(FILE*,char**);
+void conf_save(FILE*, char*, char*);
+
+void conf_read(FILE* file, char *param, char div)
+{
+  char *buffer;
+  buffer = (char*) malloc(SIZE);
+  char *line = NULL;
+  size_t len;
+  size_t read;
+  
+  while((read=getline(&line, &len, file))!=-1)
+    {
+      fputs(line,stdout);
+    }
+}
+
+
+void conf_save (FILE* pFile, char* value, char* prop)
+{
+  char * buff1=NULL;
+  char * buff2=NULL;
+  char  buff3[1000];
+  int nline;
+  buff1 = (char*) malloc(SIZE*sizeof(char));
+  buff2 = (char*) malloc(SIZE*sizeof(char));
+  read_file(pFile,&buff1);
+  strcpy(buff3,prop);
+  int loc = get_count(buff3,pFile,&nline);
+  strncpy(buff2, &buff1[nline], strlen(buff1)-nline);
+  fseek ( pFile , loc , SEEK_SET );
+  fputs (" = ", pFile );
+  fputs (value, pFile );
+  fseek (pFile, 0, SEEK_CUR);
+  fputs(buff2, pFile);
+  free(buff1);
+  free(buff2);
+  fclose ( pFile );
+}
+
+char* get_prop(char* buff)
+{
+  char *buff2 = strdup(buff);
+  char* prop = strtok(buff2, "=");
+  return prop;
+}
+
+int get_count(char* buff, FILE* fp, int *line_size)
+{
+  char* str = trim_string(buff);
+  char* line = NULL;
+  size_t len;
+  size_t read;
+  int count=0;
+  while((read=getline(&line, &len, fp))!=-1)
+    {
+      count += strlen(line);
+      if (strcmp(trim_string(get_prop(line)), str)==0)
+	{
+	  *line_size = count;
+	  count = count-strlen(line)+strlen(str);
+	  break;
+	}
+    }
+  return count;
+}
+
+char* trim_string(char* str)
+{
+  char* c = NULL;
+  c = str;
+  int i = 0;
+  while(isspace(*c)){
+    str++;
+    c++;
+  }
+  if(*c==0)
+    fputs(str,stdout);
+
+  c = str+strlen(str)-1;
+  while(c>str && isspace(*c))
+    {
+      i++;
+      c--;
+    }
+  str[strlen(str)-i] = '\0';
+  return str;
+}
+
+void read_file(FILE* fp, char** string)
+{
+  unsigned char *buffer = (char*)malloc(MAX_FILE_SIZE*sizeof(char));
+  int n;
+  if(fp){
+    n = fread(buffer, MAX_FILE_SIZE, 1, fp);
+  }else{
+    fputs("Error reading file", stderr);
+  }
+  strcpy(*string,buffer); 
+  rewind(fp);
+  free(buffer);
+}
+
